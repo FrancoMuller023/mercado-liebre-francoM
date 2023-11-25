655e081c736e036ba9e83339b35e42fa4ef37b78
francomuller023 <mullerfranco997@gmail.com>
2023-11-25 14:45:48 -0300

    Cambiando cuestiones para render

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ let path = require("path");
 
 app.use('/public', express.static(path.join(__dirname,'public')))
 
-app.listen(3000, console.log("Servidor corriendo en: http://localhost:3000/")); 
+let port = process.env.PORT || 3000;
+app.listen(port, console.log("Servidor corriendo en: http://localhost:3000/")); 
 
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "./views/index.html"));
